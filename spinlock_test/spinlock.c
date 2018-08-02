47ae47170302406462315dd7aca8d91b1c476f04
Suff3r <1610613243@qq.com>
2018-08-02 00:12:21 -0700

    1) spin_lock compare with mutex cost time 2) critical section cost less time: spin_lock have a better performance than mutex, but spin_lock only can be used in preemptive multi-cpu environment,when critical section cost more time, mutex is better,so choose mutex in most occasion

---
+++ spinlock_test/spinlock.c
@@ -0,0 +1,69 @@
+/*************************************************************************
+    > File Name: spinlock.c
+    > Author: ma6174
+    > Mail: ma6174@163.com 
+    > Created Time: Wed 01 Aug 2018 08:47:13 PM PDT
+ ************************************************************************/
+/*
+ * >do what :compare spin_lock with mutex about timecost
+*/
+#include<stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <sys/time.h>
+#include <unistd.h>
+
+static int num = 0;
+static pthread_spinlock_t spinlock;
+
+int get_current_timestamp(){
+	struct timeval now = {0, 0};
+	gettimeofday(&now,NULL);
+	return now.tv_sec * 1000 * 1000 + now.tv_usec;
+}
+
+void *thread_proc(void *arg){
+	int i = 0;
+	int j = 0;
+	for (;i < 1000000; ++i){
+		pthread_spin_lock(&spinlock);
+		++num;
+		for (j = 0; j < 100; ++j){
+			//do nothing
+			//but make spin_lock cost more time than mutex
+		}
+		pthread_spin_unlock(&spinlock);
+	}
+}
+
+int main(){
+	pthread_t thread1,thread2;
+	pthread_spin_init(&spinlock,PTHREAD_PROCESS_PRIVATE);//maybe PHREAD_PROCESS_PRIVATE or PTHREAD_PROCESS_SHARED
+	int start = get_current_timestamp();
+	int ret = -1;
+	int end = 0;
+
+	ret = pthread_create(&thread1,NULL,(void *)thread_proc,NULL);
+	if (-1 == ret){
+		printf("thread1 create fail\n");
+	}
+	ret = pthread_create(&thread2,NULL,(void *)thread_proc,NULL);
+	if (0 != ret){
+		printf("thread2 create fail\n");
+	}
+	ret = pthread_join(thread1,NULL);
+	if (0 != ret){	
+		printf("thread1 join fail\n");
+	}
+	ret = pthread_join(thread2,NULL);
+	if (0 != ret){	
+		printf("thread2 join fail\n");
+	}
+
+	printf("num = %d\n",num);
+	end = get_current_timestamp();
+	printf("cost = %d\n",end - start);
+	
+	pthread_spin_destroy(&spinlock);
+}
+
